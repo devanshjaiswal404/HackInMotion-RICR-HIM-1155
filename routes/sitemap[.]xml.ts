cc62033a4b2f470bfa8dc564c75155ae18692c2c
himanshiparaste404 <himanshiparaste404@gmail.com>
2026-08-14 17:53:25 +0530

    Update sitemap[.]xml.ts

--- routes/sitemap[.]xml.ts
+++ routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://spend-bud-buddy.lovable.app";
+const BASE_URL = "https://sightfinai.lovable.app";
 
 interface SitemapEntry {
   path: string;
